96e0994e143383b65298fb6a2e3f2e690a61ee05
sanjay adhith <60250789+tfmsadhith@users.noreply.github.com>
2026-03-20 17:57:07 +0800

    Improved unit-testing.

--- 5.1/inversion.scm
+++ 5.1/inversion.scm
@@ -1,4 +1,6 @@
-;; Time-stamp: <2026-03-20 12:42:46 kim>
+;; Time-stamp: <2026-03-20 17:52:03 kim>
+
+;; Run on Chez Scheme 10.2.0
 
 ;; Following along section 5.1.1 of Knuth
 
@@ -10,9 +12,26 @@
 ;; The output is the corresponding
 ;; permutation.
 
-;; This algorithm is in O(n), but it is
-;; indispensable for debugging purposes.
+(define (test-is-inversion-table? candidate)
+  (and (equal? (candidate '())  #t)
+       (equal? (candidate '(0)) #t)
+       (equal? (candidate '(2 1 0)) #t)
+       (equal? (candidate '(2 0 0)) #t)
+       (equal? (candidate '(1 0 0)) #t)
+       (equal? (candidate '(1 1 0)) #t)
+       (equal? (candidate '(3 2 1 0)) #t)
+       (equal? (candidate '(2 2 1 0)) #t)
+       (equal? (candidate '(1 2 1 0)) #t)
+       (equal? (candidate '(2 1 2)) #f)
+       (equal? (candidate '(2 0 2)) #f)
+       (equal? (candidate '(1 0 2)) #f)
+       (equal? (candidate '(1 1 2)) #f)
+       (equal? (candidate '(3 2 1 4)) #f)
+       (equal? (candidate '(2 2 3 0)) #f)
+       (equal? (candidate '(1 3 1 0)) #f)))
 
+;; This is in O(n), but it is indispensable for
+;; debugging purposes.
 (define (is-inversion-table? table)
   (letrec ([visit (lambda (table n)
 		    (if (null? table)
@@ -22,9 +41,31 @@
 			     (visit (cdr table) (- n 1)))))])
     (visit table (- (length table) 1))))
 
+(unless (time (test-is-inversion-table? is-inversion-table?))
+  (errorf 'is-inversion-table? "failed its unit tests."))
+
 (define (test-inv-to-perm candidate)
   (and (equal? (candidate '()) '())
-       (equal? (candidate (list 0)) (list 0))
+       (equal? (candidate '(0)) '(0))
+       (equal? (candidate '(0 0)) '(0 1))
+       (equal? (candidate '(0 0 0)) '(0 1 2))
+       (equal? (candidate '(0 0 0 0)) '(0 1 2 3))
+       (equal? (candidate '(0 0 0 0 0)) '(0 1 2 3 4))
+       (equal? (candidate '(0 0 0 0 0 0)) '(0 1 2 3 4 5))
+       (equal? (candidate '(0)) '(0))
+       (equal? (candidate '(1 0)) '(1 0))
+       (equal? (candidate '(2 1 0)) '(2 1 0))
+       (equal? (candidate '(3 2 1 0)) '(3 2 1 0))
+       (equal? (candidate '(4 3 2 1 0)) '(4 3 2 1 0))
+       (equal? (candidate '(5 4 3 2 1 0)) '(5 4 3 2 1 0))
+       (equal? (candidate '(4 2 2 1 0)) '(4 3 1 2 0))
+       (equal? (candidate '(3 2 1 1 0)) '(4 2 1 0 3))
+       (equal? (candidate '(7 7 2 2 0 0 1 1 0)) '(4 5 2 3 8 6 7 0 1))
+       (equal? (candidate '(0 1 2 3 4 3 2 1 0)) '(0 8 1 7 2 6 3 5 4))
+       ;; These are from exercise 5.1.1.1.
+       (equal? (candidate '(5 0 1 2 1 2 0 0)) '(1 6 2 4 3 0 7 5))
+       (equal? (candidate '(2 0 5 2 2 3 0 0 0)) '(1 6 0 7 3 4 8 2 5))
+       ;; This is the example in the text Knuth uses.
        (equal? (candidate (list 2 3 6 4 0 2 2 1 0)) (list 4 8 0 7 1 5 3 6 2))))
 
 (define (insert vs i n)
@@ -47,7 +88,8 @@
 		  (iota (length table)))
       (errorf 'inv-to-perm "that was not an inversion table.")))
 
-(time (test-inv-to-perm inv-to-perm))
+(unless (time (test-inv-to-perm inv-to-perm))
+  (errorf 'inv-to-perm "failed its unit tests."))
 
 ;; Bird-Wadler duality, insert is left-permutative.
 ;; We'd prefer fold-left over fold-right to avoid
@@ -64,17 +106,19 @@
 		 (reverse (iota (length table))))
       (errorf 'inv-to-perm-2 "that was not an inversion table.")))
 
-
-(time (test-inv-to-perm inv-to-perm-2))
+(unless (time (test-inv-to-perm inv-to-perm-2))
+  (errorf 'inv-to-perm-2 "failed its unit tests."))
 
 ;; Fusing reverse and iota.
 
 (define (count-down n)
   (if (< n 0)
       (errorf 'count-down "only takes in non-negative inputs")
-      (if (zero? n)
-	  '()
-	  (cons (- n 1) (count-down (- n 1))))))
+      (letrec ([visit (lambda (i acc)
+			(if (= n i)
+			    acc
+			    (visit (+ i 1) (cons i acc))))])
+	(visit 0 '()))))
 
 (define (inv-to-perm-3 table)
   (if (is-inversion-table? table)
@@ -85,7 +129,8 @@
 		 (count-down (length table)))
       (errorf 'inv-to-perm-3 "that was not an inversion table.")))
 
-(time (test-inv-to-perm inv-to-perm-3))
+(unless (time (test-inv-to-perm inv-to-perm-3))
+  (errorf 'inv-to-perm-3 "failed its unit tests."))
 
 ;; All of the above algorithms are in O(n^2)
 ;; there exist O(nlogn) algorithms based on
